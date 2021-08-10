6c7bd6a77cfe96ee0a1ccb2fad2addb58eec4f20
MeenMus <thebroski9999@gmail.com>
2021-08-10 12:25:28 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['middleware' => ['auth','admin']], function() {
+
+    /* tset */
     Route::get('create-file',[CreateFileController::class, 'create']);
     Route::post('create-file',[CreateFileController::class, 'store']);
 
